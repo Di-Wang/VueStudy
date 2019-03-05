ff338dfdfca8cbb5a78d6ae4023791e112e8d174
Di-Wang <2609457135@qq.com>
2019-03-05 14:57:00 +0800

    base222 add vue-split-table

---
+++ VUEStudy/VueBase222/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Table />
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld'
+  import Events from './components/Event'
+  import Formdata from './components/Formdata'
+  import Styles from './components/style'
+  import Table from './components/table'
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+      Events,
+      Formdata,
+      Styles,
+      Table
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  img {
+    width: 100px;
+  }
+</style>
---
+++ VUEStudy/VueBase222/src/components/Event.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="event">
+        <ul>
+            <li @click.once="getItemInfo(index,$event)" v-for="(name, index) in names" :key="index">
+                {{ name }}
+            </li>
+        </ul>
+
+        <input type="text" v-on:keyup.enter="getKeyInfo">
+
+        <button v-on:click="pushArr">按钮</button>
+
+        <button @click="getOdd">我要奇数</button>
+        <ul>
+            <li v-for="num in wantNums">{{ num }}</li>
+        </ul>
+
+        <div>
+            <p>{{ msgRe }}</p>
+            <p>{{ msgRe }}</p>
+            <p>{{ methodsMsgRe() }}</p>
+        </div>
+    </div>    
+</template>
+<script>
+
+export default{
+    //data中的数据改变会引起视图的改变  MVVM
+    data() {
+        return {
+            names: ["iwen", "ime", "ice"],
+            nums: [1, 2, 3, 4, 5, 6, 6, 7],
+            wantNums:[],
+            message: "hello"
+        }
+    },
+    methods: {
+        getItemInfo(data, event) {
+            //事件中的默认参数event
+            console.log(this.names[data])
+            console.log(event);
+        },
+        getKeyInfo(event) {
+            console.log(event);
+        },
+        pushArr() {
+            // this.names.push("frank");
+            this.names.concat(["frank"]);
+        },
+        getOdd() {
+            this.nums.filter(num => {
+                if (num % 2 !== 0) {
+                    this.wantNums.push(num);
+                }
+            })
+        },
+        methodsMsgRe() {
+            return this.message.split('').reverse().join('');
+        }
+    },
+    computed: {
+        msgRe() {
+            return this.message.split('').reverse().join(''); 
+        }
+    },
+}
+
+</script>
+<style>
+
+</style>
